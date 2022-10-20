f2f1d93589890286d0cdb85397204d857249d0ff
Felipe Castro <111576084+felipejmcastro@users.noreply.github.com>
2022-10-20 01:18:42 +0000

    Exibindo mensagem com variaveis

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 # Meu primeiro projeto Python!!
-# print() = comando de saida
+
 print("HelloWorld")
+
+
+nome = "Felipe Castro"
+idade = 19
+
+print(nome)
+
+print("Eu tenho " +str(idade) + " anos")
+print("Eu tenho {} anos".format(idade))
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
+
+
+
+
 
 
 
